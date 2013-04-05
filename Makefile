169e3826a102869e6390af11cc2bd1ce1fe803f8
Hai Lin <hai.lin.cs@gmail.com>
2013-04-05 11:06:09 -0400

    #1 recruiter can post job

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = g++
 CFLAGS = -g   -I gtest-1.6.0/include mybuild/libgtest.a -lpthread 
 PROG =object_calisthenics_hai 
-SRCS=recruitertest.cpp 
+SRCS=recruitertest.cpp recruiter.cpp 
 
 
 all: $(PROG)
--- recruitertest.cpp
+++ recruitertest.cpp
@@ -1,6 +1,13 @@
 #include <gtest/gtest.h>
+#include "Recruiter.h"
 
 TEST(EmptyTest, emptytest) {
+
+}
+
+TEST(RecruiterCanPostJob, postJob) {
+	Recruiter* recruiter = new Recruiter();
+	Job* job = recruiter->postJob();
 }
 
 int main(int argc, char **argv) {

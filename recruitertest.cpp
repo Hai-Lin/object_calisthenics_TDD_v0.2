cd9f9635be567aaed8eeb4bef232d62d743695e3
Hai Lin <hai.lin.cs@gmail.com>
2013-04-05 12:06:57 -0400

    #2 test pass

--- recruitertest.cpp
+++ recruitertest.cpp
@@ -26,6 +26,18 @@ TEST(RecruiterCanSeeAListOfJobTheyPost, postOneJob) {
 	delete recruiter;
 }
 
+TEST(RecruiterCanSeeAListOfJobTheyPost, postMutipleJob) {
+	Recruiter* recruiter = new Recruiter();
+	Job* job1 = recruiter->postJob();
+	Job* job2 = recruiter->postJob();
+	Job* job3 = recruiter->postJob();
+	JobCollection* jobCollection = recruiter->getAllJobs();
+	EXPECT_EQ(jobCollection->atIndex(0), job1);
+	EXPECT_EQ(jobCollection->atIndex(1), job2);
+	EXPECT_EQ(jobCollection->atIndex(2), job3);
+	delete recruiter;
+}
+
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest( &argc, argv );
 	return RUN_ALL_TESTS();

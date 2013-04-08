a0754183ffe3b13b14ca301216627edefee9fa4a
Hai Lin <hai.lin.cs@gmail.com>
2013-04-07 23:02:31 -0400

    #3 refactor

--- JobSeeker.cpp
+++ JobSeeker.cpp
@@ -1,6 +1,20 @@
 #include"JobSeeker.h"
 
+JobSeekerJobs::JobSeekerJobs() {
+	vector<Job*> jobs;
+	_jobs = jobs;
+}
+
+void JobSeekerJobs::addJob(Job* job) {
+	_jobs.push_back(job);
+}
+
+JobSeeker::JobSeeker() {
+	JobSeekerJobs* jobSeekerJobs = new JobSeekerJobs();
+	_savedJobs = jobSeekerJobs;
+}
+
 void JobSeeker::saveJob(Job* job) {
-	_savedJobs.push_back(job);
+	_savedJobs->addJob(job);
 }
 
--- JobSeeker.h
+++ JobSeeker.h
@@ -1,8 +1,19 @@
 #include"utility.h"
 class Job;
+
+class JobSeekerJobs
+{
+	private:
+		vector<Job*> _jobs;
+	public:
+		JobSeekerJobs();
+		void addJob(Job*);
+};
+
 class JobSeeker {
 	private:
-		vector<Job*> _savedJobs;
+		JobSeekerJobs* _savedJobs;
 	public:
+		JobSeeker();
 		void saveJob(Job* );
 };

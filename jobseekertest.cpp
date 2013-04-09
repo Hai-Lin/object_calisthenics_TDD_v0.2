0ed43e4747e6098628cbb2f4e74398cd7b0b0d69
Hai Lin <hai.lin.cs@gmail.com>
2013-04-08 22:11:58 -0400

    clean up

--- jobseekertest.cpp
+++ jobseekertest.cpp
@@ -18,6 +18,31 @@ TEST(JobSeekerHaveResume, haveResumeTest) {
 	EXPECT_FALSE(jobSeekerWithoutResume->hasResume());
 }
 
+TEST(JobSeekersCanApplyJob, applyOneJob) {
+	JobSeeker* jobSeekerWithResume = createJobSeekerWithResume();
+	JobSeeker* jobSeekerWithoutResume = createJobSeekerWithoutResume();
+	Job* atsJob = createATSJob();
+	Job* JReqJob = createJReqJob();
+	JobSeekerApplyJob* jobSeekerApplyJob = new JobSeekerApplyJob();
+	EXPECT_TRUE(jobSeekerApplyJob->applyJob(jobSeekerWithResume, atsJob));
+	EXPECT_TRUE(jobSeekerApplyJob->applyJob(jobSeekerWithResume, JReqJob));
+	EXPECT_TRUE(jobSeekerApplyJob->applyJob(jobSeekerWithoutResume, atsJob));
+	EXPECT_FALSE(jobSeekerApplyJob->applyJob(jobSeekerWithoutResume, JReqJob));
+}
+
+TEST(JobSeekersCanSaveJob, savejob) {
+	JobSeeker* jobSeekerWithResume = createJobSeekerWithResume();
+	JobSeeker* jobSeekerWithoutResume = createJobSeekerWithoutResume();
+	Job* atsJob = createATSJob();
+	Job* JReqJob = createJReqJob();
+	JobSeekerSaveJob* jobSeekerSaveJob = new JobSeekerSaveJob();
+	jobSeekerSaveJob->saveJob(jobSeekerWithResume, atsJob);
+	jobSeekerSaveJob->saveJob(jobSeekerWithResume, JReqJob);
+	jobSeekerSaveJob->saveJob(jobSeekerWithoutResume, JReqJob);
+	jobSeekerSaveJob->saveJob(jobSeekerWithoutResume, atsJob);
+}
+
+
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest( &argc, argv );
 	return RUN_ALL_TESTS();
